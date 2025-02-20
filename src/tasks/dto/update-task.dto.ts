c18f1a5a69e32c88a9cf4041c122557199c074dd
Samuel Oliveira <samueldsoliveira7@gmail.com>
2025-02-20 16:13:52 -0300

    adds suporte to mapped types and data validation in the tasks updates

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,14 +1,8 @@
-import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTaskDto } from './create-task.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateTaskDto {
-  @IsString()
-  @IsOptional()
-  name?: string;
-
-  @IsString()
-  @IsOptional()
-  description?: string;
-
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   @IsBoolean()
   @IsOptional()
   completed?: boolean;
