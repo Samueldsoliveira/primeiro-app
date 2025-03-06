9ad202a07b734957b7f6217e18e5982aa8de84b4
Samuel Oliveira <samueldsoliveira7@gmail.com>
2025-03-06 14:19:33 -0300

    implements CRUD to users module with data validation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(id: number) {
+    try {
+      const user = await this.prisma.user.findFirst({
+        where: { id },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+
+      if (!user) {
+        throw new HttpException(
+          `User with id ${id} not found`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return user;
+    } catch (error) {
+      console.error(`Error finding user with id ${id}:`, error);
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          name: createUserDto.name,
+          email: createUserDto.email,
+          passwordHash: createUserDto.password,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      console.error(error);
+
+      throw new HttpException(
+        'Falha ao cadastrar usuário!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async updateUser(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      const user = await this.prisma.user.update({
+        where: {
+          id: id,
+        },
+        data: {
+          email: updateUserDto.email,
+          name: updateUserDto.name,
+          passwordHash: updateUserDto.password,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Falha ao atualizar usuário!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async deleteUser(id: number) {
+    try {
+      const user = await this.prisma.user.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      if (!user) {
+        throw new HttpException(
+          `User with id ${id} not found`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return {
+        message: `User with id ${id} deleted successfully`,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Falha ao deletar usuário!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
